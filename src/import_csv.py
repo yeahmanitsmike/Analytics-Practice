d60b1896b2f5096fab493949fe31b826b5679984
yeahmanitsmike <yeahmanitsmike@gmail.com>
2025-03-02 15:39:25 -0600

    sunday afternoon commit. Did some more practice and now I started practicing analysis tactics. Need to figure out which direction to go. like always lol

--- src/import_csv.py
+++ src/import_csv.py
@@ -2,34 +2,35 @@ import pandas as pd
 import csv
 import time
 import json
+import xml.etree.ElementTree as ET
 
 start_time = time.time()
 path = '/home/mike-rob/Development/Analytic_Dev/data/Vendor_Payments.csv'
     
 
-def load_csv_to_dict_pd(file_path: str) -> list[dict]:
+def load_csv_to_dict_pd(file_path: str) -> None:
     df = pd.read_csv(file_path)
     df = df.to_dict(orient="records")
-    return df
+    print(df[:2])
 
-def print_dict_pd() -> None:
-    dict = load_csv_to_dict_pd(path)
-    print(dict[:2])
-
-def load_csv_to_dict_csv(file_path: str) -> list[dict]:
+def load_csv_to_dict_csv(file_path: str) -> None:
     with open(file_path, mode="r", newline="", encoding="utf-8") as file:
         reader = csv.DictReader(file)
-        data = [row for row in reader]
-        return data
+        data = [row for _, row in zip(range(5), reader)]
+        print(data[:2])
 
-def print_dict_csv() -> None:
-    dict = load_csv_to_dict_csv(path)
-    print(dict[:2])
-
-def load_csv_to_df(file_path: str) -> pd.DataFrame:
+def load_csv_to_df(file_path: str) -> None:
     df = pd.read_csv(file_path)
     df = pd.DataFrame(df)
-    return df
+    print(df.head(5))
+
+def load_manual_csv_to_dict(file_path: str) -> None:
+    with open(file_path, mode="r", newline="", encoding="utf-8") as file:
+        reader = csv.reader(file)
+        headers = next(reader)
+        data = [dict(zip(headers, row)) for row in reader]
+        print(data[:2])
+        # Total run time:  0.7601 seconds
 
 # loads a csv using csv module
 def load_csv_to_csv(file_path: str) -> None:
@@ -59,15 +60,33 @@ def csv_to_json_json(file_path: str) -> None:
         print(jobj)
     # 0.0003 runtime
 
+def csv_to_xml(file_path: str, root_element: str = "root", row_element: str = "row") -> None:
+    root = ET.Element(root_element)
+
+    with open(file_path, mode="r", newline="", encoding="utf-8") as file:
+        reader = csv.DictReader(file)
+        for i, row in enumerate(reader):
+            if i > 5:
+                break
+            row_ele = ET.SubElement(root, row_element)
+            for key, value in row.items():
+                child = ET.SubElement(row_ele, key)
+                child.text = value
+
+    xml = ET.tostring(root, encoding="utf-8", method="xml").decode()
+    print(xml)
+# Total run time:  0.0006 seconds
+
 
 if __name__ == "__main__": 
     
-    # print(load_csv_to_df(path).head(5)) 
-    # print_dict_pd()
-    # print_dict_csv()
+    # csv_to_xml(path)
+    # load_manual_csv_to_dict(path)
+    # load_csv_to_dict_csv(path)
+    # load_csv_to_df(path)
     # load_csv_to_csv(path)
-    # csv_to_json_pandas(path)
-    csv_to_json_json(path)
+    csv_to_json_pandas(path)
+    # csv_to_json_json(path)
 
     end_time = time.time()
     time_elapse = end_time - start_time
--- src/import_json.py
+++ src/import_json.py
@@ -1,10 +1,45 @@
 import json
 import pandas as pd
+import time
+
+start_time = time.time()
 
 path = '/home/mike-rob/Development/Analytic_Dev/data/Vendor_List.geojson'
 
-def load_json_to_json():
-    pass
+def load_json_to_pd(file_path: str) -> None:
+    with open(file_path, mode="r") as file:
+        data = json.load(file)
+    
+    df = pd.json_normalize(data["features"])
+    print(df.head())
+    # Total run time:  0.0278 seconds
+
+def load_json_with_json(file_path: str) -> None:
+    with open(file_path, "r") as file:
+        vendor_data = json.load(file)
+
+    features = vendor_data.get("features", [])
+
+    for i, feat in enumerate(features[:5]):
+        print(f"feature {1+i}\n")
+        print(json.dumps(feat, indent=2))
+    # Total run time:  0.0064 seconds
+
+def json_to_csv(file_path: str) -> None:
+    with open(file_path, mode="r") as file:
+        data = json.load(file)
+        data = data["features"]
+    df = pd.json_normalize(data)
+    # df.to_csv(save_path, index=False)
+    print(df)
+
+
 
 if __name__ == "__main__":
-    pass
+    # load_json_to_pd(path)
+    # load_json_with_json(path)
+    json_to_csv(path)
+    
+    end_time = time.time()
+    time_elapse = end_time - start_time
+    print(f"Total run time: {time_elapse: .4f} seconds")
