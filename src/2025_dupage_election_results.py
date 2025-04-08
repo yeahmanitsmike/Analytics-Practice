386a1bec57d38f65a6b661d6971995105d847b16
yeahmanitsmike <yeahmanitsmike@gmail.com>
2025-04-07 21:30:42 -0500

    switched up the xml read to xml etree library and things are moving slow but better than pandas.  also pivoted off of jupyter notebooks for easy editing

---
+++ src/2025_dupage_election_results.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib as plt
+import xmltodict
+import xml.etree.ElementTree as ET
+
+
+in_file = "/home/mike-rob/Development/Analytic_Dev/data/detail.xml"
+
+tree = ET.parse(in_file)
+root = tree.getroot()
+
+timestamp = root.findtext("Timestamp") 
+election_name = root.findtext("ElectionName")
+election_date = root.findtext("ElectionDate")
+region = root.findtext("Region")
+
+# print(f"{timestamp=}, {election_name=}, {election_date=}, {region=}")
+
+metadata = {
+        "Timestamp": timestamp,
+        "Election Name" : election_name,
+        "Election Date" : election_date,
+        "Region" : region
+    }
+
+# print(metadata)
+
+agg_root = root.find("VoterTurnout")
+
+agg_voter_data = {
+    "Total Voters": int(agg_root.attrib["totalVoters"]),
+    "Ballots Cast": int(agg_root.attrib["ballotsCast"]),
+    "Voter Turnout": float(agg_root.attrib["voterTurnout"])
+}
+
+# print(f"{agg_voter_data=}")
+
+metadata["Total Voters"] = agg_voter_data['Total Voters']
+metadata["Ballots Cast"] = agg_voter_data['Ballots Cast']
+metadata["Voter Turnout"] = agg_voter_data['Voter Turnout']
+
+print(f"{metadata=}")
+
+precinct_turnout = []
+for precinct in root.find("./VoterTurnout/Precincts"):
+    precinct_turnout.append({
+        "Precinct Name": precinct.attrib.get("name"),
+        "Total Voters": precinct.attrib.get("totalVoters"),
+        "Ballots Cast": precinct.attrib.get("ballotsCast"),
+        "Voter Turnout": precinct.attrib.get("voterTurnout")
+    })
+
+print(f"{precinct_turnout[:4]=}")
